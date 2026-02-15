2920272a5fb70ea8da56adfffe88f3e0500deb5e
abijith2109 <abijithraghu007@gmail.com>
2026-02-15 23:09:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("question.html")
+
 @app.route("/question")
 def question():
     return render_template("question.html")
-
-@app.route("/letter")
-def letter():
-    return render_template("letter.html")
-
-@app.route("/gift")
-def gift():
-    return render_template("gift.html")
 
 @app.route("/choice")
 def choice():
@@ -22,13 +18,13 @@ def choice():
 def hug():
     return render_template("hug.html")
 
-@app.route("/kiss")
-def kiss():
-    return render_template("kiss.html")
+@app.route("/gift")
+def gift():
+    return render_template("gift.html")
 
-@app.route("/final")
-def final():
-    return render_template("final.html")
+@app.route("/letter")
+def letter():
+    return render_template("letter.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
